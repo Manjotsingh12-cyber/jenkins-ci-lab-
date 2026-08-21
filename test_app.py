c35fd60e3f705b245e6b459fc9795b4077874a3c
Manjotsingh12-cyber <mbrar9766@gmail.com>
2026-08-21 10:47:48 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
 from app import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello from Jenkins" in response.data
+
 
 def test_health():
     client = app.test_client()
